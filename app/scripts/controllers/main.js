a76e132ff0afeea3dc1f6c413882c0fd8f72afdf
Nicholas Kristoffersen <nick@kristoffersen.io>
2017-03-27 20:21:38 -0700

    slowing down capture

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -116,13 +116,13 @@ angular.module('angularSimplesuspectApp')
 				console.log(resp.data);
 				setTimeout(function () {
 					$scope.makeSnapshot();
-				}, 500);
+				}, 5000);
 			}, function (resp) {
 				$scope.person = '';
 				$scope.status = 'noface';
 				setTimeout(function () {
 					$scope.makeSnapshot();
-				}, 500);
+				}, 5000);
 			});
 
 
